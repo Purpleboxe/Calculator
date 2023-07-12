c7a9e9bbb1d93862e1351d5a4b1569d063557e7d
Antonio <antoniozamora21331@gmail.com>
2023-07-11 17:39:10 -0700

    Add some visuals like the calculator itself

---
+++ app.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNumber = 0;
+let operator = '';
+let secondNumber = 0;
+
+function operate(firstNumber, operator, secondNumber) {
+    switch(operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+            break;
+        case '-':
+            return subtract(firstNumber, secondNumber);
+            break;
+        case '*':
+            return multiply(firstNumber, secondNumber);
+            break;
+        case '/':
+            return divide(firstNumber, secondNumber);
+            break;
+        default:
+            return 'Invalid Operator';
+            break;
+    }
+}
