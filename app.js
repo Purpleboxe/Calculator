6e627325ec7a116732a8795c5ec0aca9252763f1
Antonio <antoniozamora21331@gmail.com>
2023-07-14 18:25:51 -0700

    Get the calculator to properly function

--- app.js
+++ app.js
@@ -1,39 +1,123 @@
-function add(a, b) {
-    return a + b;
+let firstNumber = '';
+let operator = null;
+let secondNumber = '';
+
+let reset = false;
+
+const display = document.getElementById('display');
+
+// Get all the buttons
+const numberBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operator]');
+const equalBtn = document.getElementById('equal');
+const clearBtn = document.getElementById('clear');
+const deleteBtn = document.getElementById('delete');
+const decimalBtn = document.getElementById('decimal');
+
+numberBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        setNumber(button.value);
+    })
+})
+
+operatorBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        setOperation(button.value);
+    })
+})
+
+equalBtn.addEventListener('click', evaluate);
+clearBtn.addEventListener('click', clear);
+deleteBtn.addEventListener('click', deleteNumber);
+decimalBtn.addEventListener('click', decimal);
+
+function setNumber(number) {
+    // If there is a 0 on the screen or reset is true then remove the number currently on the screen
+    if (display.textContent === '0' || reset) {
+        resetScreen();
+    }
+    // Make sure the display doesn't have more than 12 characters
+    if (display.textContent.length < 12) {
+        display.textContent += number;
+    }
 }
 
-function subtract(a, b) {
-    return a - b;
+function setOperation(setOperator) {
+    if (operator !== null) {
+        evaluate();
+    }
+    // Set the first number and operator
+    firstNumber = display.textContent;
+    operator = setOperator;
+    reset = true;
 }
 
-function multiply(a, b) {
-    return a * b;
+function evaluate() {  
+    if (operator === null || reset) {
+        return;
+    }
+    if (operator === '÷' && display.textContent === '0') {
+      alert("You can't divide by 0!");
+      return;
+    }
+    // Set the second number and then display the answer
+    secondNumber = display.textContent;
+    display.textContent = roundResult(operate(firstNumber, secondNumber, operator));
+    operator = null;
+    display.textContent = display.textContent.slice(0, 12);
 }
 
-function divide(a, b) {
-    return a / b;
+function roundResult(n) {
+    return Math.round(n * 1000) / 1000;
 }
 
-let firstNumber = 0;
-let operator = '';
-let secondNumber = 0;
+function clear() {
+    // Set everything to default
+    display.textContent = '0';
+    firstNumber = '';
+    operator = null;
+    secondNumber = '';
+}
 
-function operate(firstNumber, operator, secondNumber) {
+function deleteNumber() {
+    // When deleting numbers make sure that if there are no numbers 0 is always present
+    if (display.textContent !== '0') {
+        display.textContent = display.textContent.slice(0, -1);
+        if (display.textContent === '') {
+            display.textContent += '0';
+        }
+    }
+}
+
+function decimal() {
+    if (display.textContent.includes('.')) {
+        return;
+    }
+    display.textContent += '.';
+}
+
+function resetScreen() {
+    display.textContent = '';
+    reset = false;
+}
+
+function operate(x, y, operator) {
+    x = Number(x);
+    y = Number(y);
     switch(operator) {
         case '+':
-            return add(firstNumber, secondNumber);
-            break;
+            return x + y;
         case '-':
-            return subtract(firstNumber, secondNumber);
-            break;
-        case '*':
-            return multiply(firstNumber, secondNumber);
-            break;
-        case '/':
-            return divide(firstNumber, secondNumber);
-            break;
+            return x - y;
+        case '×':
+            return x * y;
+        case '÷':
+            if (y === 0) {
+                return null;
+            } else {
+                return x / y;
+            }
         default:
-            return 'Invalid Operator';
-            break;
+            return 'null';
     }
 }
